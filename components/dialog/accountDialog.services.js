a2c37aecac5461aa2d469545d7f7c747a255ab6f
nextabc <wawyhow@gmail.com>
2017-01-11 10:59:48 +0700

    Revert "first"
    This reverts commit e849530f4606d9d6f514a3d4b33b8e53ba76832e.

--- components/dialog/accountDialog.services.js
+++
@@ -1,7 +0,0 @@
-angular.module('vietteltvServices').service('AccountDialogService', AccountDialogService);
-
-AccountDialogService.$inject = ['$http', '$q', '$resource', 'Constant'];
-
-function AccountDialogService($http, $q, $resource, Constant) {
-
-}
--- components/dialog/dialog.services.js
+++
@@ -1,8 +0,0 @@
-angular.module('vietteltvServices')
-        .service('DialogService', DialogService);
-
-DialogService.$inject = ['$http', '$q', '$resource', 'Constant'];
-
-function DialogService($http, $q, $resource, Constant) {
-
-}
